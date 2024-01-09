6f9c38047f7deb2a216d4cec14d5adf33f0dda73
tochicode <josephudokae@gmail.com>
2024-01-09 14:05:13 +0100

    adding selector files

---
+++ cypress/e2e/selector.cy.js
@@ -0,0 +1,7 @@
+const url = 'https://katalon-demo-cura.herokuapp.com';
+const username = 'John Doe';
+const password = 'ThisIsNotAPassword';
+const facility = 'Seoul CURA Healthcare Center';
+const comment = 'Please I would like my consulting doctor be Dr. Lee Jun Hee';
+
+export {url, username,password, facility, comment}
